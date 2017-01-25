e7e9e28aa645b69e7fc7ae8857f6d887b072dfc3
Nicolas Clairon <nicolas.clairon@zerosix.com>
2017-01-25 13:56:00 +0100

    fix path for production settings

--- moneyland/settings/prod.py
+++ moneyland/settings/prod.py
@@ -3,7 +3,7 @@ from .common import *
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-STATIC_ROOT = "/home/moneyland/moneyland/public"
+STATIC_ROOT = "/home/namlook/moneyland/public"
 STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
 
 PIPELINE['CSS_COMPRESSOR'] = 'pipeline.compressors.NoopCompressor'
